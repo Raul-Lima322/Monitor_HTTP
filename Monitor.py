d30942fba3a3154b2f13ef56f7ffb4901ecc5cd8
Raul-Lima322 <raulpinheirolima@gmail.com>
2026-07-13 17:20:48 -0300

    Ajustes para melhorar a estabilidade do programa

--- Monitor.py
+++ Monitor.py
@@ -6,18 +6,35 @@ contadorGeral = 0
 contadorSucesso = 0
 contadorPerdido = 0
 
+if os.name == "nt":
+    os.system("cls")
+else :   
+    os.system("clear")
 while True:
-    urlUser = input("Digite um url: ").strip()
+    urlUser = input("Digite um url (ex: youtube.com): ").strip().lower()
 
     if not urlUser:
         print("[!] Valor esperado. ")
         continue
-    
-    escolha = input("1- Https\n2- Http\n").strip()
 
-    if escolha not in ["1", "2"]:
+    if urlUser.startswith("https://"):
+        urlUser = urlUser.replace("https://", "")
+    elif urlUser.startswith("http://"):
+        urlUser = urlUser.replace("http://", "")
+    
+    escolhaProtocolo = input("1- Https\n2- Http\n").strip()
+
+    if escolhaProtocolo not in ["1", "2"]:
         print("[!] Escolha de protocolo é necessária (1 ou 2)")
         continue
+
+    if escolhaProtocolo == "1": 
+        protocolo = "https://"
+    elif escolhaProtocolo == "2":
+        protocolo = "http://"
+            
+
+    urlFinal = protocolo + urlUser
     break
 
 try:
@@ -25,12 +42,7 @@ try:
 
     while True: 
         try:
-            if escolha == "1": 
-                url = requests.get("https://" + urlUser, auth=('user', 'pass'), timeout=5)
-            elif escolha == "2":
-                url = requests.get("http://" + urlUser, auth=('user', 'pass'), timeout=5)
-            
-         
+            url = requests.get(urlFinal, auth=('user', 'pass'), timeout=5)    
             time.sleep(0.3)
             
             if url.status_code == 200:
